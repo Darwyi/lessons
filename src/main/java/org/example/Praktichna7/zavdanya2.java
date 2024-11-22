01d6efd359f404e602573279f6199ee9837feac6
Zhenya Krasovskiy <zhenya20022008@gmail.com>
2024-11-22 11:05:35 +0200

    added 1 and 2 task from 7 practice

---
+++ src/main/java/org/example/Praktichna7/zavdanya2.java
@@ -0,0 +1,46 @@
+package org.example.Praktichna7;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class zavdanya2 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+
+        System.out.println("Введіть розмір двовимірного масиву: ");
+        int row_size = s.nextInt();
+        int column_size = s.nextInt();
+
+        double[][] array = new double[row_size][column_size];
+
+        for (int i = 0; i < row_size; i++){
+            for (int j =0; j < column_size; j++) {
+                array[i][j] = r.nextDouble();
+            }
+        }
+
+        for (double[] row : array) {
+            for (double num : row) {
+                System.out.printf("%.4f\t", num);
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < row_size; i++) {
+            for (int j = 0; j < column_size; j++) {
+                if (i % 2 != 0 || j % 2 != 0) {
+                    array[i][j] = Math.sqrt(array[i][j]);
+                }
+            }
+        }
+        System.out.println("Дані масиву зі змінами: \n");
+
+        for (double[] row : array) {
+            for (double num : row) {
+                System.out.printf("%.4f\t", num);
+            }
+            System.out.println();
+        }
+    }
+}
